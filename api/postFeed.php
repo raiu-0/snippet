48e6dcb3f25b1cbdb6c0a47bbd4a35805c7be9a2
raiu <paraguaraphael0913@gmail.com>
2024-07-17 20:14:27 +0800

    Restructured condition when taking in different modes.

--- api/postFeed.php
+++ api/postFeed.php
@@ -6,9 +6,6 @@ require '../utils/dateManager.php';
 
 date_default_timezone_set('Asia/Hong_Kong');
 
-if(!isset($_SESSION['user']))
-    exit();
-
 $post_input = file_get_contents('php://input');
 $data = json_decode($post_input, true);
 
@@ -16,25 +13,28 @@ function extractFiles($element){
     return $element['filename'];
 }
 
+if (!isset($_SESSION['user']))
+    exit();
+
 $con = startDBConnection();
 
-$username = $_SESSION['user'];
-$followingData = getDataByIdentifier($con, 'follow', 'account', $username, true);
-$followingList = [];
-foreach($followingData as $follow)
-    $followingList[] = $follow['followed'];
-$followingList[] = $username;
-
-if(isset($data['username']))
+if (!isset($data['username'])) {
+    $username = $_SESSION['user'];
+    $followingData = getDataByIdentifier($con, 'follow', 'account', $username, true);
+    $followingList = [];
+    foreach ($followingData as $follow)
+        $followingList[] = $follow['followed'];
+    $followingList[] = $username;
+} else
     $followingList = [$data['username']];
 
 $posts = getPosts($con, $followingList);
 
-foreach($posts as &$post){
-    $postFiles = getDataByIdentifier($con, 'post_files', 'post_id', $post['id'], true); 
+foreach ($posts as &$post) {
+    $postFiles = getDataByIdentifier($con, 'post_files', 'post_id', $post['id'], true);
     $post['postFiles'] = array_map('extractFiles', $postFiles);
     $post['datetime'] = getTimePassed($post['datetime']);
-    
+
     $acctData = getDataByIdentifier($con, 'users', 'username', $post['username']);
 
     $post['profilePicture'] = $acctData['picture'];
