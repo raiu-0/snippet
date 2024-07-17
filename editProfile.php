354b0699688605f8ef9a5e15f466c2cbc16b5166
raiu <paraguaraphael0913@gmail.com>
2024-07-17 20:09:30 +0800

    Added favicon.

--- editProfile.php
+++ editProfile.php
@@ -59,6 +59,7 @@ endDBConnection($con);
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="images/icons/snippet-icon.png">
 </head>
 
 <body>
